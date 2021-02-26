2071859e9500e9a38aa51cc7e536e0c6ae773536
Michael <tironamichael@gmail.com>
2021-02-26 12:58:41 -0800

    Added code to Sarech component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
+    const [term, setTerm] = useState('');
+
+    console.log('I run with every render');
+
+    useEffect(() => {
+        console.log('I only run once');
+    }, []);
+
     return (
         <div>
             <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term</label>
-                    <input className="input"/>
+                    <input 
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                        className="input"
+                    />
                 </div>
             </div>
         </div>
